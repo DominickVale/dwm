0a74ef5d719c85ccba824080d54284ebe52e0e8c
Dominick Vale <dominickveil@gmail.com>
2021-10-13 09:42:35 +0200

    make win0 float

--- config.h
+++ config.h
@@ -80,6 +80,7 @@ static const Rule rules[] = {
 	{ NULL,      	     "spterm",   NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      	     "spcalc",   NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
 	{ NULL,		     NULL, 	EMULATOR_MAIN_CNAME,SPTAG(2),     1,           0,         0,        -1 },
+	{ NULL,		     NULL, 	"win0",		    0,     	  1,           0,         0,        -1 },
 };
 
 /* layout(s) */
